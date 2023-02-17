06c4d79e19fdbf15c179a7b8291e88e059d8a8fc
Rokade <120449913+r0kade@users.noreply.github.com>
2023-02-17 12:43:36 +0800

    Update BaseBlock.java

--- src/wdef/blocks/basic/BaseBlock.java
+++ src/wdef/blocks/basic/BaseBlock.java
@@ -2,14 +2,17 @@ package wdef.blocks.basic;
 
 import mindustry.world.*;
 import mindustry.gen.*;
-/** A block that does nothing. */
+/** A block that does nothing. 
+ * @author Rokade
+ */
 public class BaseBlock extends Block {
   public BaseBlock(String name){
     super(name);
     solid = destructible = true;
+    canOverdrive = drawDisabled = false;
+    envEnabled = Env.any;
   }
-  
-  public class BaseBlockBuild extends Building {
+  public class BaseBuild extends Building {
     
   }
 }
