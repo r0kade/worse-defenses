38a87c98e37e14cc6d644ccee31eead0e784fd29
Rokade <120449913+r0kade@users.noreply.github.com>
2023-02-17 15:16:41 +0800

    Create Bomb.java

---
+++ src/wdef/blocks/defense/Bomb.java
@@ -0,0 +1,34 @@
+package wdef.blocks.defense;
+
+import mindustry.world.blocks.defense.*;
+import mindustry.gen.*;
+import mindustry.entities.*;
+import mindustry.content.*;
+import arc.util.*;
+import arc.audio.*;
+import wdef.blocks.*;
+
+/** A block that explodes when tapped.
+  * @author Rokade
+  */
+public class Bomb extends BaseBlock {
+  public Bomb(String name){
+    super(name);
+  }
+  public Effect explosionEffect = Fx.none;
+  public Sound explosionSound = Sounds.wind3;
+  public int explosionDamage = 100;
+  public int explosionRadius = 7;
+  
+  public class BombBuild extends BaseBuild {
+    // todo build configuration
+    public void tapped(){
+      kill();
+    }
+    
+    public void onDestroyed(){
+      explosionSound.at(x, y, Mathf.random(0.9f, 1.1f));
+      Damage.damage(this.team, x, y, explosionRadius*8, explosionDamage);
+    }
+  }
+}
