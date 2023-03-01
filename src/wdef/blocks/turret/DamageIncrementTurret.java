a5bc366110865d2941e7b337365cc56a19c26129
Rokade <120449913+r0kade@users.noreply.github.com>
2023-03-01 21:47:04 +0800

    Create DamageIncrementTurret.java

---
+++ src/wdef/blocks/turret/DamageIncrementTurret.java
@@ -0,0 +1,14 @@
+package wdef.blocks.turret;
+
+import mindustry.world.blocks.defense.turrets.*;
+
+// todo bad name
+public class DamageIncrementTurret extends PowerTurret {
+  public DamageIncrementTurret(String name){
+    super(name);
+
+  }
+  public class DamageIncrementTurretBuild extends PowerTurretBuild {
+    
+  }
+}
